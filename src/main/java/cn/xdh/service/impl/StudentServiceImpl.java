7801de6f4f2104396a7add97d7dd9f59c0143184
jie <728292125@qq.com>
2019-12-24 11:39:13 +0800

    j测试

--- src/main/java/cn/xdh/service/impl/StudentServiceImpl.java
+++ src/main/java/cn/xdh/service/impl/StudentServiceImpl.java
@@ -16,4 +16,6 @@ public class StudentServiceImpl implements StudentService {
         Student student = studentdao.selectByPhoneAndPassword(phone,password);
         return student;
     }
+
+    //j
 }
